520939233f17461d5b701cb1a314b89aed5b7128
Giulia <giulia@MacBook-Pro-di-Giulia.local>
2017-09-21 13:22:12 -0400

    Updated rules for RR too short or too long

--- Tools/PerformAFdetection.m
+++ Tools/PerformAFdetection.m
@@ -32,9 +32,10 @@ for i = 1:length(AfAnalysisWindows)
         AFtest(i) = NaN;
     else
         idx_af = find(tNN >= tstart & tNN < tstart + HRVparams.af.windowlength);
-        % When the RR interval time series is < 8 cannot extract feautures
-        if length(NN_afcalc(idx_af)) < 8 
+        % When the RR interval time series is < 12 or >60 cannot extract feautures
+        if length(NN_afcalc(idx_af)) < 12 || length(NN_afcalc(idx_af)) < 60 
             features_af = NaN;
+            AFtest(i) = NaN;
         else
             features_af = AF_features(NN_afcalc(idx_af),HRVparams.Fs);
             AFtest(i) = SVM_AFdetection_withoutTrainingModel(features_af,1);

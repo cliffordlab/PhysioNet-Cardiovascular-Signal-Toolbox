f3790d6997b048640ef8f8d40accc1e185328a34
Giulia <giulia@MacBook-Pro-di-Giulia.local>
2017-10-11 13:40:04 -0400

    used Main_VOSIM in the Demo

--- Demos/DemoAnnotatedData.m
+++ Demos/DemoAnnotatedData.m
@@ -21,11 +21,8 @@
 %       more information
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-clear all; clc; close all;
+clear; clc; close all;
 HRVparams = InitializeHRVparams('demo');   % Initialize settings for demo
-
-
-
 % Check existence of Input\Output data folders and add to search path
 
 if  isempty(HRVparams.readdata) || ~exist([pwd filesep HRVparams.readdata], 'dir')    
@@ -56,73 +53,20 @@ numsub = length(subjectIDs);
 
 for i_patient = 1:numsub   
     try
-    
+        
         %% 1. Import Patient Data
         % HRVparams = initialize_HRVparams('demo'); % enable this when using parfor loops
         RRwindowStartIndices = [];
         tNN = [];
         NN = [];
         [samples, t, rr, ~, ~, annotations, HRVparams.Fs, ~, ~] = read_qrs(filesTBA{i_patient},HRVparams.datatype);
-        %%% QUESTION: Does read_qrs accept filename on all platforms, or just
-        %%%     LINUX?
-        %%% TO DO - Improve functionality of specifying data to read function
-
-        %% 2. Preprocess RR Data - Using HRV Toolbox
-        % Remove noise, Remove ectopy, Don't detrend (yet)
-        [NN, tNN, fbeats] = RRIntervalPreprocess(rr,t,annotations, HRVparams);
-
-        %% 3. Calculate Windows
-        RRwindowStartIndices = CreateWindowRRintervals(tNN, NN, HRVparams);   
-
-        %% 4. Calculate AF Features
         
-        [AFtest, AFwindowsStartIndices] = PerformAFdetection(subjectIDs{i_patient},tNN,NN,HRVparams);
-        RRwindowStartIndices = RemoveAFsegments(RRwindowStartIndices,AFwindowsStartIndices, AFtest,HRVparams);
-        
-        %% 5. Calculate time domain HRV metrics - Using HRV Toolbox
-        [NNmean,NNmedian,NNmode,NNvariance,NNskew,NNkurt, SDNN, NNiqr, ...
-            RMSSD,pnn50,btsdet,avgsqi,fbeatw, RRwindowStartIndices] = EvalTimeDomainHRVstats(NN,tNN,[],HRVparams,RRwindowStartIndices);
+        %% 2. Perform HRV analysis on the RR intervals
+        HRVparams.MSE.on = 0; % No MSE analysis for this demo
+        [results, resFilenameHRV] = Main_VOSIM(rr,t,'RRIntervals',HRVparams,subjectIDs(i_patient),annotations);
 
-        %% 6. Frequency domain HRV metrics (LF HF TotPow)
-        %       All Inputs in Seconds
-        %%% TO DO: Remove necessity of creating phantom beats with lomb 
-
-        [ulf, vlf, lf, hf, lfhf, ttlpwr, methods, fdflag, window] = ...
-            EvalFrequencyDomainHRVstats(NN,tNN, [],HRVparams,RRwindowStartIndices);
-
-        %% 7. PRSA
-        try
-            [ac,dc,~] = prsa(NN, tNN, [], RRwindowStartIndices, HRVparams);
-        catch
-            ac = NaN; 
-            dc = NaN;
-            error_flag(i_patient) = subjectids(i_patient);
-        end
-
-        %% 8. SDANN and SDNNi
-        [SDANN, SDNNI] = CalcSDANN(RRwindowStartIndices, tNN, NN(:),HRVparams);
-
-   
-        %% 9. Save Results
-        % Uncomment the following lines for All Results
-        results = [RRwindowStartIndices(:),ac(:),dc(:),...
-            ulf(:),vlf(:),lf(:),hf(:),lfhf(:),ttlpwr(:),fdflag(:),...
-            NNmean(:),NNmedian(:),NNmode(:),...
-            NNvariance(:),NNskew(:),NNkurt(:),SDNN(:),NNiqr(:),RMSSD(:),pnn50(:),btsdet(:),fbeatw(:)];
-        
-        col_titles = {'t_win','ac','dc','ulf','vlf','lf','hf','lfhf',...
-            'ttlpwr','fdflag','NNmean','NNmedian','NNmode','NNvar','NNskew',...
-            'NNkurt','SDNN','NNiqr','RMSSD','pnn50','beatsdetected','corrected_beats'};
-
-        % Uncomment the following lines for just Mean
-        % results = [NNmean(:)];
-        % col_titles = {'NN Mean'};
-
-        % Generates Output - Never comment out
-        resFilenameHRV = GenerateHRVresultsOutput(subjectIDs(i_patient), ...
-            RRwindowStartIndices,results,col_titles, [], HRVparams, tNN, NN);
               
-        %% 10 Compare generated output file with the reference one
+        %% 3. Compare generated output file with the reference one
         
         currentFile = [HRVparams.writedata filesep resFilenameHRV '.csv'];
         referenceFile = ['ReferenceOutput' filesep 'Annotated_HRV_allwindows.csv'];
@@ -130,8 +74,6 @@ for i_patient = 1:numsub
         
         if testHRV
             fprintf('** DemoAnnotatedData: TEST SUCCEEDED ** \n ')
-             fprintf('A file named %s.csv \n has been saved in %s \n', ...
-            resFilenameHRV, HRVparams.writedata);
         else
             fprintf('** DemoAnnotatedData: TEST FAILED ** \n')
         end

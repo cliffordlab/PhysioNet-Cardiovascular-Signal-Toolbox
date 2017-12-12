7e2c55e97c0ed33650bf334a159f65a9decf16a9
Giulia <giulia@MacBook-Pro-di-Giulia.local>
2017-12-12 10:31:48 -0500

    Changed variables names to make the fucntion clearer

--- Tools/Entropy_Tools/RR_Preprocessing_for_MSE.m
+++ Tools/Entropy_Tools/RR_Preprocessing_for_MSE.m
@@ -1,103 +1,84 @@
-function [ v1, tv1] = RR_Preprocessing_for_MSE( NN, tNN )
-%RR_PREPROCESSING_FOR_MSE Summary of this function goes here
-%   
+function [ RR_gapFilled, t_gapFilled] = RR_Preprocessing_for_MSE( rr, t )
+%
+% [ RR_gapFilled, t_gapFilled] = RRIntervalPreprocess(rr,t);
+% This function deals with missing data in rr time series. When rr
+% intervals is 'too' long, i.e., grather that 2 times the median value of
+% the 10 rr inetrvals before and after, this function will fill the gap
+% with median values
+%
+% Written by Giulia Da Poian (giulia.dap@gmail.com) and Pradyumna Suresha
+% (pradyumna.suresha@gmail.com)
 
-%[NN, tNN, flagged_beats] = RRIntervalPreprocess(RR,tRR,annotations,HRVparams);
 
-tv1 = [];
-v1 = [];
-
-
-th = 10;
+t_gapFilled = [];
+RR_gapFilled = [];
 
 
-% NN = flipud(NN(:));
-% tNN = flipud(tNN(:));
-v = abs(diff(tNN));
+NmbOfBeatsForMedian = 10;
+
+tmp_RR_gapFilled = abs(diff(t));
+
 ii = 0;
-for idx = 1:length(v)
+for idx = 1:length(tmp_RR_gapFilled)
     try
-        medianValue = median(NN(idx-th: idx+th));
+        medianValue = median(rr(idx-NmbOfBeatsForMedian: idx+NmbOfBeatsForMedian));
     catch 
         try
-            medianValue = median(NN(idx-th: idx));
+            medianValue = median(rr(idx-NmbOfBeatsForMedian: idx));
         catch
             try
-                medianValue = median(NN(idx:idx+th));
+                medianValue = median(rr(idx:idx+NmbOfBeatsForMedian));
             catch
-                medianValue = median(NN(1:idx));
+                medianValue = median(rr(1:idx));
             end
         end
     end
     
     thresh = 2 * medianValue;
-    
-       if(min(v1)<0)
-           5
-       end        
+       
             
-    if v(idx) > thresh
+    if tmp_RR_gapFilled(idx) > thresh
        
-       samps = floor(v(idx)/medianValue); 
-       
-%        tNN(idx+1)
-       
-       v1 = [v1 ; NN(idx)];
-       tv1 = [tv1; tNN(idx)];
+       MissingBeats = floor(tmp_RR_gapFilled(idx)/medianValue); 
+              
+       RR_gapFilled = [RR_gapFilled ; rr(idx)];
+       t_gapFilled = [t_gapFilled; t(idx)];
        ii=ii+1;
        % adding 'samps' points
-       tv1 = [tv1; ones(samps,1)];
-       v1 = [v1; ones(samps,1)];
-       ii=ii+samps;
-       tv1(end) = tNN(idx+1) - NN(idx+1);
-       v1(end) = medianValue;
+       t_gapFilled = [t_gapFilled; ones(MissingBeats,1)];
+       RR_gapFilled = [RR_gapFilled; ones(MissingBeats,1)];
+       ii=ii+MissingBeats;
+       t_gapFilled(end) = abs(t(idx+1) - rr(idx+1));
+       RR_gapFilled(end) = medianValue;
        
-       for k = 1: samps-1
+       for k = 1: MissingBeats-1
        
-           tv1(end-k) = tv1(end-k+1) - v1(end-k+1);
-           v1(end-k) = medianValue;
+           t_gapFilled(end-k) = abs(t_gapFilled(end-k+1) - RR_gapFilled(end-k+1));
+           RR_gapFilled(end-k) = medianValue;
        
        end
        
        % chack if fisrt value in the gap is correct
        
-       if tv1(ii-samps+1) - tNN(idx) < 0.5*medianValue
-           tv1(ii-samps+1) = [];
-           v1(ii-samps+1) = [];
+       if abs(t_gapFilled(ii-MissingBeats+1) - t(idx)) < 0.5*medianValue
+           t_gapFilled(ii-MissingBeats+1) = [];
+           RR_gapFilled(ii-MissingBeats+1) = [];
            ii=ii-1;
-           samps = samps -1;
+           MissingBeats = MissingBeats -1;
        end   
-       v1(ii-samps+1) = tv1(ii-samps+1) - tNN(idx);
-           
+       RR_gapFilled(ii-MissingBeats+1) = abs(t_gapFilled(ii-MissingBeats+1) - t(idx));
 
-
-       
-%         v1 = [v1; NN(idx) ];
-%         tv1 = [tv1; tNN(idx); tNN(idx)-medianValue];
-%         
-%         for k = 1:samps-1          
-%             v1 = [v1; medianValue];
-%             tv1 = [tv1; tv1(end)-medianValue];
-%         end
-%         
-%         if abs(tNN(idx+1)-tv1(end)) <= 0.5*medianValue
-%             tv1(end)=[];
-%             v1(end) = abs(tNN(idx+1)-tv1(end));
-%         else
-%             v1 =[v1; abs(tNN(idx+1)-tv1(end))];
-%         end
-%    
         
 
     else
-        v1 = [v1 ; NN(idx)];
-        tv1 = [tv1; tNN(idx)];
+        RR_gapFilled = [RR_gapFilled ; rr(idx)];
+        t_gapFilled = [t_gapFilled; t(idx)];
         ii=ii+1;
     end
 
 end
 
-v1 = [v1; NN(end)];
-tv1 = [tv1; tNN(end)];
+RR_gapFilled = [RR_gapFilled; rr(end)];
+t_gapFilled = [t_gapFilled; t(end)];
 
 

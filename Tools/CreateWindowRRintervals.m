1a9f3b8d6cb818f46c4e93185bd681a7f34faeb0
Giulia <giulia@MacBook-Pro-di-Giulia.local>
2018-01-05 12:00:35 +0100

    Code modified to speed up the cration of indexes

--- Tools/CreateWindowRRintervals.m
+++ Tools/CreateWindowRRintervals.m
@@ -75,7 +75,7 @@ switch option
 end
 
 % Initialize output matrix
-windowRRintervals = [];
+windowRRintervals = 0: increment: floor( (tNN(end) - windowlength + increment));
 
 % Initialize loop variables
 t_window_start = 0;     % Window Start Time
@@ -95,48 +95,45 @@ i = 1;                       % Counter
 %     end
 % end
 
+if ~strcmp(option,'af') && ~strcmp(option,'sqi')
+    
+    while t_window_start <= tNN(end) - windowlength + increment
 
-while t_window_start <= tNN(end) - windowlength + increment
-    
-    % Find indices of time values in this segment
-    idx_window = find(tNN >= t_window_start & tNN < t_window_start + windowlength);
-    t_win = tNN(idx_window);
-    
-    % if NN intervals are supplied, assign them to the current window
-    % if not, put in place a vector of ones as a place holder
-    if ~isempty(NN)
-        nn_win = NN(idx_window);
-    else
-        nn_win = (windowlength/length(t_win))* ones(length(t_win),1);
-    end
-    
-    % Store the begin time of window
-    windowRRintervals(i) = t_window_start;
-   
-    % Increment time by sliding segment length (sec)
-    t_window_start = t_window_start + increment;
-    
-    % Check Actual Window Length and mark windows that do not meet the
-    % crieria for Adequate Window Length
-    % First remove unphysiologic beats from candidates for this
-    % measurement:
-    idxhi = find(nn_win > HRVparams.preprocess.upperphysiolim);
-    idxlo = find(nn_win < HRVparams.preprocess.lowerphysiolim);
-    comb = [idxhi ; idxlo];
-    nn_win(comb) = [];
-    % Now query the true length of the window by adding up all of the NN
-    % intervals
-    truelength = sum(nn_win(:));
-    if truelength < (windowlength * (1-win_tol))
-        % Only remove window if not working in 'AF' or 'SQI' Mode
-        if ~strcmp(option,'af') && ~strcmp(option,'sqi')
-            windowRRintervals(i) = NaN; 
+        % Find indices of time values in this segment
+        t_win = tNN(tNN >= t_window_start & tNN < t_window_start + windowlength);
+
+        % if NN intervals are supplied, assign them to the current window
+        % if not, put in place a vector of ones as a place holder
+        if ~isempty(NN)
+            nn_win = NN(tNN >= t_window_start & tNN < t_window_start + windowlength);
+        else
+            nn_win = (windowlength/length(t_win))* ones(length(t_win),1);
         end
-    end
-    
-    % Increment loop index
-    i = i + 1;
-end
 
+        % Store the begin time of window
+        windowRRintervals(i) = t_window_start;
+
+        % Increment time by sliding segment length (sec)
+        t_window_start = t_window_start + increment;
+
+        % Check Actual Window Length and mark windows that do not meet the
+        % crieria for Adequate Window Length
+        % First remove unphysiologic beats from candidates for this
+        % measurement:
+        idxhi = find(nn_win > HRVparams.preprocess.upperphysiolim);
+        idxlo = find(nn_win < HRVparams.preprocess.lowerphysiolim);
+        comb = [idxhi ; idxlo];
+        nn_win(comb) = [];
+        % Now query the true length of the window by adding up all of the NN
+        % intervals
+        truelength = sum(nn_win(:));
+        if truelength < (windowlength * (1-win_tol))
+                windowRRintervals(i) = NaN; 
+        end
+
+        % Increment loop index
+        i = i + 1;
+    end
+end
 
 end % end of function

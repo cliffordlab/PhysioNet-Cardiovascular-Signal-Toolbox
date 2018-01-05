85b9f795ca1ecae73cd0a3f6a33f58015c3ef089
Giulia <giulia@MacBook-Pro-di-Giulia.local>
2018-01-05 12:04:40 +0100

    code vectorized

--- Tools/ECG_Analysis_Tools/AF Feature Calculation/ReGet_min_numerator.m
+++ Tools/ECG_Analysis_Tools/AF Feature Calculation/ReGet_min_numerator.m
@@ -14,19 +14,33 @@ function [Min_numerator,r]=ReGet_min_numerator(x1,x2,N,m,r,fs)
 %           Institute of Biomedical Engineering,
 %           Shandong University
 % $Last updated:  2015.10.10
-% Las updated: 2017.19.12 (by Giulia Da Poian) d2max preallocation
+% Las updated: 2017.19.12 (by Giulia Da Poian) vectorized
 
-d2max = zeros(N-m, N-m);
+dim = N-m;
 
-for i=1:N-m
-    Min_numerator=0;
-    for j=1:N-m
-        d2max(i,j)=max(abs(x2(i,:)-x2(j,:)));
-        if  d2max(i,j)<=r
-            Min_numerator=Min_numerator+1;
-        end
-    end
-end
+% Fast implementation
+
+t1 = repmat(x2(:,1),[1 dim]);
+t2 = toeplitz([x2(1,1); flipud(x2(2:end,1))], x2(:,1));
+t3 = repmat(x2(:,2),[1 dim]);
+t4 = toeplitz([x2(1,2); flipud(x2(2:end,2))], x2(:,2));
+d2max = max(abs(t1-t2),abs(t3-t4));
+d2max = cell2mat(arrayfun(@(x) circshift(d2max(x,:),[1 dim-x+1]),(1:dim)','un',0));
+Min_numerator = numel(find(d2max(dim,:)<=r));
+
+
+% Old implementation (slower)
+% % for i=1:N-m
+% %     Min_numerator=0;
+% %     for j=1:N-m
+% %         d2max(i,j)=max(abs(x2(i,:)-x2(j,:)));
+% %         if  d2max(i,j)<=r
+% %             Min_numerator=Min_numerator+1
+% %         end
+% %     end
+% % end
+
+
 if N<20
     r=r+1;
 else

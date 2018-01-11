5b742dd0d3bd2ebcec00b29a607799658af3d047
Giulia <giulia@MacBook-Pro-di-Giulia.local>
2018-01-11 13:55:20 -0500

    updated comments

--- Main_VOSIM.m
+++ Main_VOSIM.m
@@ -10,7 +10,7 @@ function [results, ResultsFileName ] = Main_VOSIM(InputSig,t,InputFormat,HRVpara
 %       InputSig    - Vector containing RR intervals data (in seconds) 
 %                     or ECG/PPG waveform  
 %       t           - Time indices of the rr interval data (seconds) or
-%                     live empty for ECG/PPG input
+%                     leave empty for ECG/PPG input
 %       InputFormat - String that specifiy if the input vector is: 
 %                     'RRIntervals' for RR interval data 
 %                     'ECGWaveform' for ECG waveform

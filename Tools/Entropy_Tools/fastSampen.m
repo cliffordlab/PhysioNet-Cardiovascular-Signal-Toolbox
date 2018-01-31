f22fc9cb4ed5dd370cff44b8a834b16493ecd149
Giulia <giulia@MacBook-Pro-di-Giulia.local>
2018-01-31 15:02:57 -0500

    Fixed bug: replaced template used to compue A and B

--- Tools/Entropy_Tools/fastSampen.m
+++ Tools/Entropy_Tools/fastSampen.m
@@ -7,16 +7,32 @@ function s = fastSampen(y,m,r)
 %  s     Sample Entropy
 %
 % Written by Shamim Nemati <shamim.nemati@alum.mit.edu>
+% 01-31-2018 : modified by Giulia Da Poian <giulia.dap@gmail.com>, see
+%              comments in the code 
+%              NOTE : Sample entropy quantifies the likelihood that a 
+%              sequence of m consecutive data points?that matches another
+%              sequence of the same length (match within a tolerance of r) 
+%              ? will still match the other sequence when their length is 
+%              increased of one sample (sequences of length m + 1); 
+%              References : 
+%              1. Richman JS, Moorman JR. Physiological time-series 
+%              analysis using approximate entropy and sample entropy. 
+%              American Journal of Physiology-Heart and Circulatory 
+%              Physiology. 2000 Jun 1;278(6):H2039-49.
+%              2. Humeau-Heurtier A. The multiscale entropy 
+%              algorithm and its variants: A review. Entropy. 2015 May 
+%              12;17(5):3110-23.
+
 
 % Ensure y is a row vector
 if size(y, 1) > size(y,2)
     y = y';
 end
 
-xx = convert_to_lagged_form(y, m-1)';
+xx = convert_to_lagged_form(y, m)';  % replaced m-1 with m
 Dxx = pdist(xx,'chebychev');
 
-yy = convert_to_lagged_form(y, m)';
+yy = convert_to_lagged_form(y, m+1)'; % replaced m with m+1
 Dyy = pdist(yy,'chebychev');
 
 A = mean( Dxx < r ) ;

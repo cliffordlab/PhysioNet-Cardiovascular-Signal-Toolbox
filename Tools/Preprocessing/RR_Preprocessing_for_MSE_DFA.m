adad3ca56e62224f0595bedb4ff15bf01f7bace1
Giulia <giulia@MacBook-Pro-di-Giulia.local>
2018-04-26 15:35:45 -0400

    removed median method - new method coming soon

--- Tools/Preprocessing/RR_Preprocessing_for_MSE_DFA.m
+++ Tools/Preprocessing/RR_Preprocessing_for_MSE_DFA.m
@@ -2,17 +2,17 @@ function [ RR_gapFilled, t_gapFilled] = RR_Preprocessing_for_MSE_DFA(rr, t )
 %
 %    [ RR_gapFilled, t_gapFilled] = RR_Preprocessing_for_MSE_DFA(rr, t )
 %
-%   OVERVIEW:   This function deals with missing data in rr time series. 
-%               When rr intervals is 'too' long, i.e., grather that 2 times
-%               the median value of the 10 rr inetrvals before and after, 
-%               this function will fill the gap with median values
-%
+%   OVERVIEW:   This function deals with missing data in rr time series for 
+%               long range HRV measure
+%               
+%               
 %   INPUT:      rr            - a single row of rr interval data in seconds
 %               t             - the time indices of the rr interval data 
 %                                 (seconds)
 %   OUTPUT:     RR_gapFilled   - RR intervals with gaps filled with median
 %                                values
 %               t_gapFilled    - time indices of RR_gapFilled
+%
 %   DEPENDENCIES & LIBRARIES:
 %       PhysioNet Cardiovascular Signal Toolbox
 %       https://github.com/cliffordlab/PhysioNet-Cardiovascular-Signal-Toolbox
@@ -34,75 +34,7 @@ function [ RR_gapFilled, t_gapFilled] = RR_Preprocessing_for_MSE_DFA(rr, t )
 %       more information
 %
 
-t_gapFilled = [];
-RR_gapFilled = [];
+t_gapFilled = t;
+RR_gapFilled = rr;
 
-
-NmbOfBeatsForMedian = 10;
-
-tmp_RR_gapFilled = abs(diff(t));
-
-ii = 0;
-for idx = 1:length(tmp_RR_gapFilled)
-    try
-        medianValue = median(rr(idx-NmbOfBeatsForMedian: idx+NmbOfBeatsForMedian));
-    catch 
-        try
-            medianValue = median(rr(idx-NmbOfBeatsForMedian: idx));
-        catch
-            try
-                medianValue = median(rr(idx:idx+NmbOfBeatsForMedian));
-            catch
-                medianValue = median(rr(1:idx));
-            end
-        end
-    end
-    
-    thresh = 2 * medianValue;
-       
-            
-    if tmp_RR_gapFilled(idx) > thresh
-       
-       MissingBeats = floor(tmp_RR_gapFilled(idx)/medianValue); 
-              
-       RR_gapFilled = [RR_gapFilled ; rr(idx)];
-       t_gapFilled = [t_gapFilled; t(idx)];
-       ii=ii+1;
-       % adding 'samps' points
-       t_gapFilled = [t_gapFilled; ones(MissingBeats,1)];
-       RR_gapFilled = [RR_gapFilled; ones(MissingBeats,1)];
-       ii=ii+MissingBeats;
-       t_gapFilled(end) = abs(t(idx+1) - rr(idx+1));
-       RR_gapFilled(end) = medianValue;
-       
-       for k = 1: MissingBeats-1
-       
-           t_gapFilled(end-k) = abs(t_gapFilled(end-k+1) - RR_gapFilled(end-k+1));
-           RR_gapFilled(end-k) = medianValue;
-       
-       end
-       
-       % chack if fisrt value in the gap is correct
-       
-       if abs(t_gapFilled(ii-MissingBeats+1) - t(idx)) < 0.5*medianValue
-           t_gapFilled(ii-MissingBeats+1) = [];
-           RR_gapFilled(ii-MissingBeats+1) = [];
-           ii=ii-1;
-           MissingBeats = MissingBeats -1;
-       end   
-       RR_gapFilled(ii-MissingBeats+1) = abs(t_gapFilled(ii-MissingBeats+1) - t(idx));
-
-        
-
-    else
-        RR_gapFilled = [RR_gapFilled ; rr(idx)];
-        t_gapFilled = [t_gapFilled; t(idx)];
-        ii=ii+1;
-    end
-
-end
-
-RR_gapFilled = [RR_gapFilled; rr(end)];
-t_gapFilled = [t_gapFilled; t(end)];
-
-
+% NOTE: 26 April 2018 This function is under development and will be released soon

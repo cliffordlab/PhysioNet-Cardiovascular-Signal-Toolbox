5c8e11c09652d00b78a40a45977e722cbab44721
Giulia Da Poian <giulia.dap@gmail.com>
2018-05-01 16:25:56 -0400

    Update PVC_detect.m
    Modified to accept as input raw ECG file, independent from WFDB

--- Tools/ECG_Analysis_Tools/PVCdetection/PVC_detect.m
+++ Tools/ECG_Analysis_Tools/PVCdetection/PVC_detect.m
@@ -1,20 +1,39 @@
-function PVC_detect(path,record,FS,wrann_text,th)
-% input: path - path of record
-%        record - WFDB record name
-%        FS - sampling freqency of record, may be overwritten by WFDB 
-%           functions
-%        wrann_text - write annotation by a text file for long data 
-%           (if wrann failed)
-%        th - threshold for QRS detection (default: 0.1), if too many
-%           missing beats, decrease th; if too many extra beats, 
-%           increase th
-% version: 7v
+function PVCs = PVC_detect(signal,Fs,th,sigName,OutputFolder)
 %
-% Emailed from Qiao Li to Adriana Vest on May 30th 2017
-% Modified July 21, 2017 to update for compatibility with the HRV toolbox
-% Modifications include all things commented with ANV
+% PVC_detect(signal,Fs,sigName,OutputFolder,th)
+% INPUTS: 
+%        signal       : Nx1 raw ech signal (in mV)
+%        Fs           : sampling freqency of record
+%        th           : threshold for QRS detection (default: 0.1), if too
+%                       many missing beats, decrease th; if too many extra 
+%                       beats, increase th
+%        sigName      : recording name
+%        OutputFolder : directory to save the annotation files 
+
+% OUTPUTS : 
+%         PVCs        : locations of detected PVC beats, in samples 
 %
-% LICENSE:    
+%   DEPENDENCIES & LIBRARIES:
+%       PhysioNet Cardiovascular Signal Toolbox
+%       https://github.com/cliffordlab/PhysioNet-Cardiovascular-Signal-Toolbox
+%
+%   REFERENCE: 
+%   Vest et al. "An Open Source Benchmarked Toolbox for Cardiovascular 
+%   Waveform and Interval Analysis" Physiological Measurement (In Press), 2018. 
+%
+%	REPO:       
+%       https://github.com/cliffordlab/PhysioNet-Cardiovascular-Signal-Toolbox%   
+%   ORIGINAL SOURCE AND AUTHORS:     
+%       Written by Qiao Li 
+%       Dependent scripts written by various authors 
+%       (see functions for details)      
+%
+%    Modified by Giulia Da Poian on May 2018 to update for compatibility 
+%    with the toolbox
+%
+%
+%	COPYRIGHT (C) 2018 
+%   LICENSE:    
 %       This software is offered freely and without warranty under 
 %       the GNU (v3 or later) public license. See license file for
 %       more information
@@ -22,103 +41,69 @@ function PVC_detect(path,record,FS,wrann_text,th)
 qrs_times=[];
 pvc_outputs=[];
 
+
+
 if nargin<5
+    OutputFolder = pwd;
+end
+if nargin<4
+    sigName = 'Test';
+end
+if nargin<3
     th=0.1;
 end
 
-if nargin<4
-    wrann_text=0;
+if nargin<2
+    Fs=360;
 end
 
-if nargin<3
-    FS=360;
-end
-
-% Create parallel pool 
-%poolobj = parpool;   % ANV commented out
-
-display('Read data ...');
-
-[siginfo,Fs] = wfdbdesc([path record]);
-
-% ANV: added the following for records that do not have a header file:
-if isempty(siginfo)
-    [tm, signal] = rdsamp([path record], [1]); 
-    siginfo(1).LengthSamples = length();
-end
-
-if isempty(Fs)
-    Fs=FS;
-end
-Fs=Fs(1);
+LengthSamples =  length(signal);
 
 % split long data to one hour each
-t_hours=siginfo(1).LengthSamples/(Fs*60*60);
-t_days= floor(t_hours/24);
-if rem( siginfo(1).LengthSamples,(Fs*60*60)) > 0    % if not exactly x hours
-    remflag=1;
+t_hours = LengthSamples/(Fs*60*60);
+
+if rem( LengthSamples,(Fs*60*60)) > 0    % if not exactly x hours
+    remflag = 1;
 else
-    remflag=0;
+    remflag = 0;
 end
 
-overlap=10; % Adding 10 seconds to the front of each hour, and throwing away the last 5 and first 5 of each hour for edge effects
-overlap_2=overlap/2;
+overlap = 10; % Adding 10 seconds to the front of each hour, and throwing away the last 5 and first 5 of each hour for edge effects
+overlap_2 = overlap/2;
 
-X = sprintf('Analyzing %d hours of data',t_hours);
-disp(X)
-signals=[];
-s_start=1;
-if t_hours >= 1 % ANV added statement to enable processing on records < 1 hour
-    parfor j=1:t_hours
-    X = sprintf('Analyzing hour number %d',j);
-    disp(X)
-    s_start= (j-1) * 200 *60 *60 + 1;
-    s_end= j * 200 *60 *60;
-    % removed Fs from rdsamp return to avoid Fs reading by mistake
-    % read the first [1] channel of data
-    [tm, signal]=rdsamp([path record], [1], s_end, s_start); 
-%     if isempty(Fs)
-%         Fs=FS;
-%     end
 
-    %signal=signal(:,1);
-    signal(find(isnan(signal)))=0;
+if t_hours >= 1 
+    for j=1:t_hours
+        s_start= (j-1) * 200 *60 *60 + 1;
+        signal(find(isnan(signal)))=0;
 
-    % edge processing
-    if j==1
-        edge_signal=ones(overlap_2*Fs,1)*median(signal); % we add half of overlap data before the first hour data
+        % edge processing
+        if j==1
+            edge_signal = ones(overlap_2*Fs,1)*median(signal); % we add half of overlap data before the first hour data
+        end
+        signal = [edge_signal; signal];
+
+        % call detectpvc()
+        [qrs_time, pvc_output] = detectpvc2(signal,Fs,th);
+
+        % edge processing
+        valid_time=intersect(find(qrs_time>overlap_2*Fs),find(qrs_time<=length(signal)-overlap_2*Fs));
+        qrs_time=qrs_time(valid_time)-overlap_2*Fs;
+        pvc_output=pvc_output(valid_time);
+        edge_signal=signal(end-overlap*Fs+1:end);
+
+        if j==1
+            qrs_time=qrs_time+ (s_start-1) ;  % correct for start of signal to current segment offset
+        else
+            qrs_time=qrs_time+ (s_start-1) - overlap_2*Fs ;  % correct for start of signal to current segment offset
+        end
+        qrs_times= ([qrs_times qrs_time]);
+        pvc_outputs= ([pvc_outputs pvc_output]);
     end
-    signal=[edge_signal; signal];
-
-    % call detectpvc()
-    [qrs_time pvc_output] = detectpvc2(signal,Fs,th);
-
-    % edge processing
-    valid_time=intersect(find(qrs_time>overlap_2*Fs),find(qrs_time<=length(signal)-overlap_2*Fs));
-    qrs_time=qrs_time(valid_time)-overlap_2*Fs;
-    pvc_output=pvc_output(valid_time);
-    edge_signal=signal(end-overlap*Fs+1:end);
-    
-    if j==1
-        qrs_time=qrs_time+ (s_start-1) ;  % correct for start of signal to current segment offset
-    else
-        qrs_time=qrs_time+ (s_start-1) - overlap_2*Fs ;  % correct for start of signal to current segment offset
-    end
-        
-
-    qrs_times= ([qrs_times qrs_time]);
-    pvc_outputs= ([pvc_outputs pvc_output]);
-    
-end
 end
 
-if remflag==1  % now do remainder of signal
+if remflag == 1  % now do remainder of signal
     s_start= floor(t_hours) * Fs *60 *60 + 1;
-    s_end= siginfo(1).LengthSamples-1;
-    % removed Fs from rdsamp return to avoid Fs reading by mistake
-    [tm,signal] = rdsamp([path filesep record], [1], s_end, s_start); % ANV added '/', GDP replaced '/' with filesep 
-
-    %signal=signal(:,1);
     signal(find(isnan(signal)))=0;
 
     % edge processing
@@ -128,7 +113,7 @@ if remflag==1  % now do remainder of signal
     signal=[edge_signal; signal];
 
     % call detectpvc()
-    [qrs_time pvc_output] = detectpvc2(signal,Fs,th);
+    [qrs_time, pvc_output] = detectpvc2(signal,Fs,th);
 
     % edge processing
     valid_time=find(qrs_time>overlap_2*Fs);
@@ -146,21 +131,14 @@ if remflag==1  % now do remainder of signal
     pvc_outputs= ([pvc_outputs pvc_output]);
 end
 
-display('Writing data ...');
+
+PVCs = qrs_times(find(pvc_outputs==1));
+
 % write output
-annType=[];
-annType(find(pvc_outputs==0))='N';
-annType(find(pvc_outputs==1))='V';
-num=zeros(length(qrs_times),1);
+annType = repmat('N',length(qrs_times),1);
+annType(find(pvc_outputs==1)) = 'V';
 
-if wrann_text==0
-    wrann_path([path filesep],record,'pvc_d',qrs_times,annType,'0',0,num); % ANV added '/' , GDP replaced '/' with filesep 
-else
-    wrann_by_text(Fs,record,'pvc_d',qrs_times,annType,'0',0,num);
-end
+wrann([OutputFolder filesep 'Annotation' filesep sigName],'pvc',qrs_times,annType);
 
-display('Finished.');
 
-% Delete the parallel pool
-%delete(poolobj);
 end

5b1fb335635288c8182ff6daeac605e3e551c033
Giulia <giulia@MacBook-Pro-di-Giulia.local>
2018-07-27 15:10:47 -0400

    added possibility to use variance to compute coarse grain time series

--- Tools/Entropy_Tools/coarsegrain.m
+++ Tools/Entropy_Tools/coarsegrain.m
@@ -1,11 +1,15 @@
-function scaledData = coarsegrain(data,tau)
+function scaledData = coarsegrain(data,tau,cg_moment)
 %   scaledData = coarsegrain(data,tau)
 %
 %   OVERVIEW:  generate the consecutive coarse-grained time series
 %   
 %   INPUTS:    
-%     data  : Vector containing the original seires
-%     tau   : scale factor (if tau = 1 returs the original series)
+%     data      : Vector containing the original seires
+%     tau       : scale factor (if tau = 1 returs the original series)
+%     cg_moment : [optional] - moment used to coarse-grain the time series,
+%                 by default use the mean 'mean'
+%                 Options: 'mean', 'varaince'
+%
 %   OUTPUTS:
 %     scaledData : the coarse-grained time series at the scale factor tau
 %
@@ -21,6 +25,9 @@ function scaledData = coarsegrain(data,tau)
 if nargin < 2
     error('Wrong number of input arguments');
 end
+if nargin < 3 || isempty(cg_moment)
+    cg_moment = 'mean';
+end
 
 L = length(data);
 J = fix(L/tau);
@@ -34,7 +41,14 @@ elseif tau == 1
     return
 else
     for idx=1:J
-        scaledData(idx) = mean(data((idx-1)*tau+1:idx*tau));
+        switch cg_moment
+            case 'mean'
+                scaledData(idx) = mean(data((idx-1)*tau+1:idx*tau));
+            case 'variance'
+                scaledData(idx) = var(data((idx-1)*tau+1:idx*tau));
+            otherwise % still use mean
+                scaledData(idx) = mean(data((idx-1)*tau+1:idx*tau));                
+        end
     end
 end
 

960951a3bec4bc69236091a7501050ac0ad5eee5
Giulia <giulia@MacBook-Pro-di-Giulia.local>
2018-09-06 09:41:53 -0400

    added basic demo: load an ecg and detect R  peaks

---
+++ Demos/Basic_Demo.m
@@ -0,0 +1,66 @@
+%	OVERVIEW:
+%       This basic demo will allow you to load an ECG file in matlab 
+%       compatible wfdb format, detect the locations of the R peaks,
+%       perform signal quality (SQI) analysis and plot the results.
+%
+%   OUTPUT:
+%       A figure with the loaded ECG signal and detected peaks will be
+%       generated
+%
+%   DEPENDENCIES & LIBRARIES:
+%       https://github.com/cliffordlab/PhysioNet-Cardiovascular-Signal-Toolbox
+%   REFERENCE: 
+%       Vest et al. "An Open Source Benchmarked HRV Toolbox for Cardiovascular 
+%       Waveform and Interval Analysis" Physiological Measurement (In Press), 2018. 
+%	REPO:       
+%       https://github.com/cliffordlab/PhysioNet-Cardiovascular-Signal-Toolbox
+%   ORIGINAL SOURCE AND AUTHORS:     
+%       Giulia Da Poian   
+%	COPYRIGHT (C) 2018 
+%   LICENSE:    
+%       This software is offered freely and without warranty under 
+%       the GNU (v3 or later) public license. See license file for
+%       more information
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all
+clc
+
+% Where are the data, in this demo they are located in a subfolder
+InputFolder = [pwd filesep 'TestData' filesep 'mitdb-Arrhythmia']; % path to the folder where you data are located
+SigName = '200m';
+
+% load the ecg using rdmat
+[tm,sig,Fs] = rdmat([InputFolder filesep SigName]);
+% the signal has two channels, from now on we will use just one 
+ecg = sig(:,1);
+
+% plot the signal
+figure(1)
+plot(tm,ecg);
+xlabel('[s]');
+ylabel('[mV]')
+
+
+% Detection of the R-peaks using the jqrs.m function included in the
+% toolbox, requires to set initialization parameters calling the
+% InitializeHRVparams.m function
+
+HRVparams = InitializeHRVparams('Demo');
+% set the exact sampling frequency usign the one from the loaded signal
+HRVparams.Fs = Fs;
+% call the function that perform peak detection
+r_peaks = jqrs(ecg,HRVparams);
+
+% plot the detected r_peaks on the top of the ecg signal
+figure(1)
+hold on;
+plot(r_peaks./Fs, ecg(r_peaks),'o');
+legend('ecg signal', 'detected R peaks')
+
+
+
+
+
+
+

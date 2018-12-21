1eb6d2b62cfa6b7243ddaa1855920bf578c0d186
Giulia Da Poian <gdapoia@BMIs-MacBook-Pro-2.local>
2018-12-21 11:47:14 +0100

    adding funciton help and license

--- Tools/Filtering_Tools/bp_filter_005_40_ECG.m
+++ Tools/Filtering_Tools/bp_filter_005_40_ECG.m
@@ -1,11 +1,37 @@
 function filtECG = bp_filter_005_40_ECG(ecg, fs)
 
-% filtECG = filterECG(data, fs)
-% apply a zerophase FIR filter 
-% Equiripple FIR LP & HP filters (cascaded): 70dB 0.05-40Hz 1dB ripple. 
+%   filtECG = filterECG(data, fs)
+%	OVERVIEW:
+%      Apply a zerophase FIR filter 
+%      Equiripple FIR LP & HP filters (cascaded): 70dB 0.05-40Hz 1dB ripple
 %
+%   INPUT:
+%       ecg     -  array vector containing the raw ECG signal 
+%       fs      - samoling frequency 
 %
-% Giulia Da Poian : giulia.dap@gmail.com
+%   OUTPUT
+%       filtECG - filtered signal
+%   
+%   DEPENDENCIES & LIBRARIES:
+%       PhysioNet Cardiovascular Signal Toolbox
+%       https://github.com/cliffordlab/PhysioNet-Cardiovascular-Signal-Toolbox
+%
+%   REFERENCE: 
+%   Vest et al. "An Open Source Benchmarked HRV Toolbox for Cardiovascular 
+%   Waveform and Interval Analysis" Physiological Measurement (In Press), 2018. 
+%
+%	REPO:       
+%       https://github.com/cliffordlab/PhysioNet-Cardiovascular-Signal-Toolbox
+%   ORIGINAL SOURCE AND AUTHORS:     
+%       Written by Giulia Da Poian (giulia.dap@gmail.com) 
+%       Dependent scripts written by various authors 
+%       (see functions for details)       
+%	COPYRIGHT (C) 2018 
+%   LICENSE:    
+%       This software is offered freely and without warranty under 
+%       the GNU (v3 or later) public license. See license file for
+%       more information
+
 
 
 % Generate the filters
